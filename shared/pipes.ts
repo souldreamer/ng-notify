9d362a71d183372642b2cf2eec9ccf13ef8a9ff8
Ionut Costica <ionut.costica@gmail.com>
2016-11-14 03:05:03 +0200

    feat(string parser): better string parsing
    Added better parsing for strings with variables (extracted to its own
    package (string-template-parser))
    
    closes #18

--- shared/pipes.ts
+++ shared/pipes.ts
@@ -1,50 +1,29 @@
 import { WatcherConfigurationVariables } from '../configuration/interfaces';
 import * as moment from 'moment';
 import 'moment-duration-format';
-import { log, inspect } from './logger';
+import { evaluateStringTemplate, PipeFunction } from 'string-template-parser';
 
-interface Pipe {
-	(parameters: string[]): string
-}
-
-interface PipeDictionary {
-	[pipeName: string]: Pipe
-}
-
-const PIPES: PipeDictionary = {
+const PIPES: {[pipeName: string]: PipeFunction} = {
 	'time-difference': timeDifference,
 	'time-difference-exact': timeDifferenceExact,
 	'date': showDate
 };
 
-export function pipelineApply(variableString: string, variables: WatcherConfigurationVariables): string {
-	let pipeline = variableString.split('|').map(part => part.trim().split(':').map(piece => piece.trim()));
-	let value = variables[pipeline[0][0]];
-	for (let stage = 1; stage < pipeline.length; stage++) {
-		value = PIPES[pipeline[stage][0]]([value, ...pipeline[stage].slice(1)]);
-	}
-	log('pipeline transform'.bold.blue, variableString, (value||'').bold, inspect(variables).grey);
-	return value;
-}
-
 export function replaceVariables(text: string, variables: WatcherConfigurationVariables): string {
-	return text.replace(
-		/\$\{([^}]+)}/i,
-		(match, variableString) => pipelineApply(variableString, variables)
-	);
+	return evaluateStringTemplate(text, variables, PIPES);
 }
 
-function timeDifference(parameters: string[]): string {
-	return moment(parameters[0]).fromNow(true);
+function timeDifference(value: string): string {
+	return moment(value).fromNow(true);
 }
 
-function timeDifferenceExact(parameters: string[]): string {
+function timeDifferenceExact(value: string): string {
 	// hack because type definitions for moment-duration-format don't seem to be working
-	return (<any>moment.duration(moment().diff(moment(parameters[0]))))
+	return (<any>moment.duration(moment().diff(moment(value))))
 		.format('d [days], h[h]m[m]s.S[s]');
 }
 
-function showDate(parameters: string[]): string {
-	if (parameters.length < 2) return moment(parameters[0]).format();
-	return moment(parameters[0]).format(parameters.slice(1).join(':'));
+function showDate(value: string, parameters: string[]): string {
+	if (parameters.length < 2) return moment(value).format();
+	return moment(value).format(parameters[0]);
 }
